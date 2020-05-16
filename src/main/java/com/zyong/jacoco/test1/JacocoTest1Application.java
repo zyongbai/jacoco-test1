126b534ef87bb060461d39d02efc73ad3870f67b
651196542@qq.com <651196542@qq.com>
2020-05-16 18:20:38 +0800

    jacoco-test1

---
+++ src/main/java/com/zyong/jacoco/test1/JacocoTest1Application.java
@@ -0,0 +1,13 @@
+package com.zyong.jacoco.test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacocoTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacocoTest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zyong/jacoco/test1/JacocoTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyong.jacoco.test1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacocoTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
